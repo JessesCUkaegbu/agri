78ade2b89143fd05aeac383be7e0aba4b184b150
JessesCUkaegbu <ukaegbujesses@gmail.com>
2024-08-28 15:45:11 +0000

    change was made

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from . views import MyGoogleLoginView
+# from . views import MyGoogleLoginView
 
 
 urlpatterns =  [
     path('sign_up/', views.sign_up, name="sign_up"),
     path('user_login/', views.user_login, name="login"),
     path('logout/', views.logout_view, name='logout'),
-    path('accounts/google/login/', MyGoogleLoginView.as_view(), name='google_login'),
+    # path('accounts/google/login/', MyGoogleLoginView.as_view(), name='google_login'),
 ]
